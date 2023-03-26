40554d42dfeb9009fc16eb0e1ff8bf90f58e6c1b
Aman-007 <atthakuraman@gmail.com>
2023-03-26 12:21:54 +0100

    Code Studio question to find duplicate items.
    Approach 1: Sort all elements and comapare i and i+1 element. If i==i+1 then return ar[i].
    Time complexity O(n2)
    
    Approach 2: Use counting sort Again time complexity O(n2)
    
    Approach 3: XOR
    
    a ^ a = 0
    a ^ 0 = a
    
    Step1: in ans store XOR of all integers.
    Step2: from 1 to N-1 again do XOR of ans and i
    
    we have given array will be from 1 to N-1 so we are doing  XOR of all elements and a^a^a=a

--- src/Duplicate.java
+++ src/Duplicate.java
@@ -1,5 +1,30 @@
-//CodeStudio:An array of size N is given with 1 to N-1 elements. One element is repeting. Find that element.
-//Ex: N=6 => array: 1,2,3,1,4,5 ans:1
+/*CodeStudio:An array of size N is given with 1 to N-1 elements. One element is repeting. Find that element.
+Ex: N=6 => array: 1,2,3,1,4,5 ans:1
+
+Code Studio question to find duplicate items.
+
+Approach 1: Sort all elements and comapare i and i+1 element. If i==i+1 then return ar[i].
+Time complexity O(n2)
+
+Approach 2: Use counting sort Again time complexity O(n2)
+
+Approach 3: XOR
+
+a ^ a = 0
+a ^ 0 = a
+
+Step1: in ans store XOR of all integers.
+Step2: from 1 to N-1 again do XOR of ans and i
+
+we have given array will be from 1 to N-1 so we are doing  XOR of all elements and a^a^a=a
+
+
+
+
+
+*/
+
+
 import java.util.Scanner;
 public class Duplicate {
 
