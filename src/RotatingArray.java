cc0be7bb8a4e5fe776ab826def58e46902b04bda
Aman-007 <atthakuraman@gmail.com>
2023-06-22 12:14:18 +0100

    //LC88: Rotating an array.

---
+++ src/RotatingArray.java
@@ -0,0 +1,42 @@
+//Rorating an array using extra space and TC O(n)
+import java.util.Scanner;
+public class RotatingArray {
+    public static void main(String[] args) {
+        int size, i, temp;
+        System.out.println("Enter size");
+        Scanner sc=new Scanner(System.in);
+        size=sc.nextInt();
+
+        int []ar=new int[size];
+        int []arr=new int[size];
+
+        System.out.println("Enter array elements");
+
+        for( i=0; i<size;i++)
+            ar[i]=sc.nextInt();
+
+        System.out.println("Array before rotation ");
+
+        for(i=0;i<size;i++){
+            System.out.print(ar[i]+"  ");
+        }
+
+
+        temp=ar[size-1];
+
+        for(i=0;i<size-1;i++)
+            arr[i+1]=ar[i];
+        arr[0]=temp;
+
+        ar=arr;
+        System.out.println("\n");
+        System.out.println("Array after rotation ");
+
+        for(i=0;i<size;i++){
+            System.out.print(ar[i]+"  ");
+        }
+
+
+
+    }
+}
